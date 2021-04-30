b7638356b7c5465365496e9617fc0a14cde4a55d
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-04-30 09:55:27 -0400

    Convert to Cogs

--- wiilink24.py
+++ wiilink24.py
@@ -1,2 +1,16 @@
-import src.commands
-import config
+import os
+import discord
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='/')
+
+bot.load_extension("src.commands.misc")
+bot.load_extension("src.commands.mod")
+
+bot.run(TOKEN)
