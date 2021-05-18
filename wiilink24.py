0ea1786019ca149e3228c27802f502f9c42976a8
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-05-18 10:29:19 -0400

    Update wiilink24.py

--- wiilink24.py
+++ wiilink24.py
@@ -3,15 +3,23 @@ import discord
 
 from dotenv import load_dotenv
 from discord.ext import commands
-
+from discord_slash import SlashCommand
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='/')
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
 
 bot.load_extension("src.commands.misc")
 bot.load_extension("src.commands.mod")
 bot.load_extension("src.commands.events")
+bot.load_extension("src.commands.converters")
+
+
 
 bot.run(TOKEN)
+
+
