6ab569ae9ef0d514c626ab3de5a14279a07eb75c
Oscie57 <hudso009@gmail.com>
2021-06-02 04:23:13 +0100

    Update wiilink24.py

--- wiilink24.py
+++ wiilink24.py
@@ -1,11 +1,7 @@
 import os
 import discord
-
 from dotenv import load_dotenv
 from discord.ext import commands
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,6 +12,6 @@ bot.load_extension("src.commands.mod")
 bot.load_extension("src.commands.events")
 bot.load_extension("src.commands.converters")
 
-
-
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 bot.run(TOKEN)
